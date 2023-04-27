1b9976fd7c2453dffbb536bfe15dcdde6f544b89
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-04-27 23:31:22 +0300

    update task 3

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ export const FriendListItem = ({ id, avatar, name, isOnline }) => {
     // console.log(name)
     return (
         <li className={css.item}>
-            <span className={css.status}>{isOnline}</span>
+            <span className={isOnline?`${css.status}`:`${css.isOnline}`}>{isOnline}</span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{ name}</p>
 </li>
